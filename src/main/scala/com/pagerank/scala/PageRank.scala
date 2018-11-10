6dbd12fd8bb9eaa92ec2a5fde07d429773d79446
Mas9311 <mas9311@gmail.com>
2018-11-10 16:28:09 -0700

    Ideal PageRank

--- src/main/scala/com/pagerank/scala/PageRank.scala
+++ src/main/scala/com/pagerank/scala/PageRank.scala
@@ -1,29 +1,44 @@
 package com.pagerank.scala
 
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 
 object PageRank {
 
   def main(args: Array[String]) {
-    val hdfsPath = "hdfs://phoenix:30381"
+    if (args.length < 1) {
+      System.err.println("Usage: SparkPageRank <file>")
+      System.exit(1)
+    }
 
-    //Create a SparkContext to initialize Spark
-//    val conf = new SparkConf()
-//    conf.setMaster("local")
-//    conf.setAppName("Word Count")
-//    val sc = new SparkContext(conf)
-    val sc = new SparkContext()
+    val spark = SparkSession
+      .builder
+      .appName("Spark Ideal PageRank")
+      .getOrCreate()
 
-    // Load the text into a Spark RDD, which is a distributed representation of each line of text
-    val inputFile = sc.textFile(hdfsPath + "/input/h")
+    val lines = spark.read.textFile(args(0)).rdd
 
-    //word count
-    val counts = inputFile.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
+    val links = lines.map{ s =>
+      val parts = s.split("\\s+")
+      (parts(0), parts(1))
+    }.distinct().groupByKey().cache()
+    var ranks = links.mapValues(v => 1.0)
 
-    counts.foreach(println)
-    counts.saveAsTextFile(hdfsPath + "/h")
+    for (i <- 1 to 25) {
+      val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
+          val size = urls.size
+          urls.map(url => (url, rank / size))
+      }
+      ranks = contribs.reduceByKey(_ + _)
+//      with taxation:
+//      ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
+    }
+
+    val finalRank = ranks.collect()
+
+    finalRank.foreach(tup => println(s"${tup._1} has rank:  ${tup._2} ."))
+
+    spark.stop()
+
   }
 
 }
